b59cc9066bdeb55770c1035cb3435f849c52e5c6
Benoît Clemenceau <benoit.clemenceau.zer@gmail.com>
2018-03-07 23:58:54 +0100

    Organisation en programmation modulaire + ajout de la lecture des données à l'ouverture + ajout de la sauvegarde à la fermeture
    - Les fonctions sont réparties dans plusieurs fichiers sources.
    - La lecture des données sauvegardées fonctionne bien à priori.
    - La sauvegarde ne fonctionne PAS encore.

---
+++ liste.c
@@ -0,0 +1,110 @@
+#include "header.h"
+
+///Fonction d'initialisation de la collection (retourne un pointeur sur l'ancre).
+t_collec* initCollec()
+{
+    t_collec* collec;
+    collec = (t_collec*) malloc(sizeof(t_collec));   ///On alloue dynamiquement l'ancre.
+
+    if(collec == NULL) ///Si une des deux allocs dynamiques s'est mal passé :
+    {
+        exit(EXIT_FAILURE); ///On quitte le programme.
+    }
+
+    return collec;
+}
+
+
+///Fonction d'initialisation d'une carte (retourne un pointeur sur la carte).
+t_carte* initCarte()
+{
+    t_carte* carte;
+    carte = (t_carte*) malloc(sizeof(t_carte));   ///On alloue dynamiquement l'ancre.
+
+    if(carte == NULL) ///Si l'alloc dynamique s'est mal passé :
+    {
+        exit(EXIT_FAILURE); ///On quitte le programme.
+    }
+
+    carte->next = NULL;
+
+    return carte;
+}
+
+
+///Fonction pour ajouter une carte (au début de la liste).
+void ajoutCarte(t_collec* collec)
+{
+    char transition[100];
+
+    t_carte* newCarte=NULL;
+    newCarte = (t_carte*) malloc(sizeof(t_carte));  ///Alloc dynamique d'une nouvelle carte.
+
+    if(newCarte == NULL)    ///Si l'alloc ne marche pas :
+    {
+        exit(EXIT_FAILURE); ///On quitte.
+    }
+
+    ///On va maintenant demander à l'utilisateur de rentrer les champs de la nouvelle carte :
+
+    printf(" Nom : ");
+    viderBuffer();
+    lire(transition, 100);
+    newCarte->nom = malloc((strlen(transition)+1)*sizeof(char));
+    strcpy(newCarte->nom, transition);
+
+    printf("\n Note g%cn%crale : ", 130, 130);
+    scanf("%d", &newCarte->note);
+
+    printf(" Poste : ");
+    viderBuffer();
+    lire(transition, 100);
+    newCarte->poste = (char*) malloc((strlen(transition)+1)*sizeof(char));
+    strcpy(newCarte->poste, transition);
+
+    printf(" Club : ");
+    lire(transition, 100);
+    newCarte->club = malloc((strlen(transition)+1)*sizeof(char));
+    strcpy(newCarte->club, transition);
+
+    printf(" Pays : ");
+    lire(transition, 100);
+    newCarte->pays = malloc((strlen(transition)+1)*sizeof(char));
+    strcpy(newCarte->pays, transition);
+
+    printf("Stats : \n VIT : ");
+    scanf("%d", &newCarte->stats.vit);
+    printf(" DRI : ");
+    scanf("%d", &newCarte->stats.dri);
+    printf(" TIR : ");
+    scanf("%d", &newCarte->stats.tir);
+    printf(" DEF : ");
+    scanf("%d", &newCarte->stats.def);
+    printf(" PAS : ");
+    scanf("%d", &newCarte->stats.pas);
+    printf(" PHY : ");
+    scanf("%d", &newCarte->stats.phy);
+
+    insertionDebut(collec, newCarte);
+
+}
+
+
+///Fonction d'insertion d'une carte en début de liste.
+void insertionDebut(t_collec* collec, t_carte* newCarte)
+{
+    if(collec->first == NULL)
+    {
+        collec->first = newCarte;
+        collec->last = newCarte;
+    }
+    if(collec->first != NULL)
+    {
+        newCarte->next = collec->first;
+        collec->first = newCarte;
+    }
+}
+
+
+
+
--- main.c
+++ main.c
@@ -11,6 +11,9 @@ int main()
     collec = initCollec();  ///On initialise la collection et on donne son adresse à collec.
 
 
+    lectureSauvegarde(collec);
+
+
     while(choix != 6)   ///Tant que l'utilisateur ne veut pas quitter :
     {
         choix=menu();   ///On lance le menu et on récupère le choix.
@@ -33,15 +36,15 @@ int main()
             printf("\n");
             retour=0;
             choix=0;
-break;
+            break;
         }
         //case 2 : recherche();
         //case 3 : tri();
 
         case 4 :
-            {
-                ajoutCarte(collec);
-                break;
+        {
+            ajoutCarte(collec);
+            break;
         }
 
         //case 5 : supprimer();
@@ -57,6 +60,7 @@ break;
 
     return 0;
 }
+
 
 ///Fonction d'affichage du menu (retourne le choix) :
 int menu()
@@ -107,63 +111,12 @@ int menu()
     return choix;
 }
 
-///Fonction d'initialisation de la collection (retourne un pointeur sur l'ancre) :
-t_collec* initCollec()
-{
-    //t_stats* stats;
-    //stats = (t_stats*) malloc(sizeof(t_stats));  ///On alloue dynamiquement les stats de la première carte.
-
-    t_carte* carte;
-    carte = (t_carte*) malloc(sizeof(t_carte));  ///On alloue dynamiquement la première carte.
-
-    t_collec* collec;
-    collec = (t_collec*) malloc(sizeof(t_collec));   ///On alloue dynamiquement l'ancre.
-
-
-    if(collec == NULL || carte == NULL /*|| stats == NULL*/) ///Si une des trois allocs dynamiques s'est mal passé :
-    {
-        exit(EXIT_FAILURE); ///On quitte le programme.
-    }
-
-    ///On initialise l'ancre :
-    collec->first = carte;
-    collec->last = carte;
-
-    ///On initialise la première carte :
-    char transition1[] = "Cristiano Ronaldo";
-    carte->nom = malloc((strlen(transition1)+1)*sizeof(char));
-    strcpy(carte->nom, transition1);
-
-    carte->note = 99;
-
-    char transition2[] = "AG";
-    carte->poste = malloc((strlen(transition2)+1)*sizeof(char));
-    strcpy(carte->poste, transition2);
-
-    char transition3[] = "Real Madrid CF";
-    carte->club = malloc((strlen(transition3)+1)*sizeof(char));
-    strcpy(carte->club, transition3);
-
-    char transition4[] = "Portugal";
-    carte->pays = malloc((strlen(transition4)+1)*sizeof(char));
-    strcpy(carte->pays, transition4);
-
-    ///On initialise les stats de la première carte.
-    carte->stats.vit = 98;
-    carte->stats.dri = 98;
-    carte->stats.tir = 99;
-    carte->stats.def = 50;
-    carte->stats.pas = 94;
-    carte->stats.phy = 95;
-
-    carte->next = NULL;
-
-    return collec;
-}
 
 ///Fonction d'affichage de la collection :
 void affichageCollec(t_collec* collec)
 {
+
+
     t_carte* carteActuelle = collec->first;
 
     if(collec == NULL)
@@ -171,8 +124,11 @@ void affichageCollec(t_collec* collec)
         exit(EXIT_FAILURE);
     }
 
-    while(carteActuelle != NULL)
+
+
+    while(carteActuelle->next != NULL)
     {
+
         printf("\t\t%s\n\n", carteActuelle->nom);
 
         printf(" Note g%cn%crale : %d\n", 130, 130, carteActuelle->note);
@@ -180,126 +136,12 @@ void affichageCollec(t_collec* collec)
         printf("  Club : %s\n", carteActuelle->club);
         printf("  Pays : %s\n", carteActuelle->pays);
 
-        printf(" Stats : %d VIT, %d DRI, %d TIR, %d DEF, %d PAS, %d PHY.\n\n", carteActuelle->stats.vit, carteActuelle->stats.dri, carteActuelle->stats.tir, carteActuelle->stats.def, carteActuelle->stats.pas, carteActuelle->stats.phy);
+        printf(" Stats : %d VIT, %d DRI, %d TIR, %d DEF, %d PAS, %d PHY.\n\n\n", carteActuelle->stats.vit, carteActuelle->stats.dri, carteActuelle->stats.tir, carteActuelle->stats.def, carteActuelle->stats.pas, carteActuelle->stats.phy);
 
 
         carteActuelle = carteActuelle->next;
+
     }
 }
 
-///Fonction tirée d'un utilisateur d'un forum pour vider le buffer de la saisie.
-void viderBuffer()
-{
-    int c = 0;
-    while (c != '\n' && c != EOF)
-    {
-        c = getchar();
-    }
-}
 
-///Fonction tirée d'un cours OpenOffice sur la saisie de texte sécuriséé, elle permet la suppression du "\n" capté par la fonction fgets.
-int lire(char *chaine, int longueur)
-{
-    char *positionEntree = NULL;
-
-    // On lit le texte saisi au clavier
-    if (fgets(chaine, longueur, stdin) != NULL)  // Pas d'erreur de saisie ?
-    {
-        positionEntree = strchr(chaine, '\n'); // On recherche l'"Entrée"
-        if (positionEntree != NULL) // Si on a trouvé le retour à la ligne
-        {
-            *positionEntree = '\0'; // On remplace ce caractère par \0
-        }
-        return 1; // On renvoie 1 si la fonction s'est déroulée sans erreur
-    }
-    else
-    {
-        return 0; // On renvoie 0 s'il y a eu une erreur
-    }
-}
-
-///Fonction pour ajouter une carte (au début de la liste).
-void ajoutCarte(t_collec* collec)
-{
-    char transition[100];
-
-    t_carte* newCarte=NULL;
-    newCarte = (t_carte*) malloc(sizeof(t_carte));  ///Alloc dynamique d'une nouvelle carte.
-
-    if(newCarte == NULL)    ///Si l'alloc ne marche pas :
-    {
-        exit(EXIT_FAILURE); ///On quitte.
-    }
-
-      ///On va maintenant demander à l'utilisateur de rentrer les champs de la nouvelle carte :
-
-    printf(" Nom : ");
-    viderBuffer();
-    lire(transition, 100);
-    newCarte->nom = malloc((strlen(transition)+1)*sizeof(char));
-    strcpy(newCarte->nom, transition);
-
-    printf("\n Note g%cn%crale : ", 130, 130);
-    scanf("%d", &newCarte->note);
-
-    printf(" Poste : ");
-    viderBuffer();
-    lire(transition, 100);
-    newCarte->poste = (char*) malloc((strlen(transition)+1)*sizeof(char));
-    strcpy(newCarte->poste, transition);
-
-    printf(" Club : ");
-    lire(transition, 100);
-    newCarte->club = malloc((strlen(transition)+1)*sizeof(char));
-    strcpy(newCarte->club, transition);
-
-    printf(" Pays : ");
-    lire(transition, 100);
-    newCarte->pays = malloc((strlen(transition)+1)*sizeof(char));
-    strcpy(newCarte->pays, transition);
-
-    printf("Stats : \n VIT : ");
-    scanf("%d", &newCarte->stats.vit);
-    printf(" DRI : ");
-    scanf("%d", &newCarte->stats.dri);
-    printf(" TIR : ");
-    scanf("%d", &newCarte->stats.tir);
-    printf(" DEF : ");
-    scanf("%d", &newCarte->stats.def);
-    printf(" PAS : ");
-    scanf("%d", &newCarte->stats.pas);
-    printf(" PHY : ");
-    scanf("%d", &newCarte->stats.phy);
-
-     ///Modification des pointeurs :
-     newCarte->next = collec->first;
-     collec->first = newCarte;
-
-}
-
-///Fonction de sauvegarde de la collection dans un fichier.
-void sauvegarde(t_collec* collec)
-{
-    FILE* sauvegarde;   ///Déclaration d'un pointeur sur fichier.
-    t_carte* courant;   ///Déclaration d'un pointeur sur la carte à sauvegarder.
-
-    sauvegarde = fopen("sauvegarde.txt", "w");  ///On ouvre le fichier de sauvegarde (on le crée s'il n'existe pas et on le remplace s'il existait déjà).
-
-    ///Sauvegarde de la première carte :
-
-    courant = collec->first;    ///On pointe sur la première cellule.
-
-    fprintf(sauvegarde, "%s\n%d\n%s\n%s\n%s\n%d\n%d\n%d\n%d\n%d\n%d\n", courant->nom, courant->note, courant->poste, courant->club, courant->pays, courant->stats.vit, courant->stats.dri, courant->stats.tir, courant->stats.def, courant->stats.pas, courant->stats.phy);
-
-///Sauvegarde des autres
-while(courant->next != NULL)
-{
-
-    courant=courant->next;
-    fprintf(sauvegarde, "%s\n%d\n%s\n%s\n%s\n%d\n%d\n%d\n%d\n%d\n%d\n", courant->nom, courant->note, courant->poste, courant->club, courant->pays, courant->stats.vit, courant->stats.dri, courant->stats.tir, courant->stats.def, courant->stats.pas, courant->stats.phy);
-
-}
-
-fclose(sauvegarde);
-
-}
